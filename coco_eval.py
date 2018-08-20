70ff1874f9553717e62f72f18fda36f20cd280e3
王广义 <x5675602@gmail.com>
2018-08-20 18:06:04 +0800

    Update coco_eval.py

--- coco_eval.py
+++ coco_eval.py
@@ -3,132 +3,135 @@ import numpy as np
 import cv2
 
 def iou(r1, r2):
-	
+    '''计算限位框r1与限位框r2之间的iou
+    '''
 	intersect_w = np.maximum(np.minimum(r1[0]+r1[2], r2[0]+r2[2])-np.maximum(r1[0], r2[0]),0)
 	intersect_h = np.maximum(np.minimum(r1[1]+r1[3], r2[1]+r2[3])-np.maximum(r1[1], r2[1]),0)
 	area_r1 = r1[2]*r1[3]
 	area_r2 = r2[2]*r2[3]
-	intersect = intersect_w*intersect_h	
+	intersect = intersect_w * intersect_h	
 	union = area_r1 + area_r2 - intersect
 	
 	return intersect/union
 
 def softmax(x):
-
+'''定义softmax函数
+'''
     e_x = np.exp(x)
     return e_x/np.sum(e_x)
 
 def sigmoid(x):
-
-	return 1.0/(1.0 + np.exp(-x))
+'''定义sigmoid函数
+'''
+    return 1.0/(1.0 + np.exp(-x))
 
 def preprocess_yolo_output(data):
-	
-	locations = []
-	classes = []
-	for i in range(grid_h):
-		for j in range(grid_w):
-			for k in range(n_anchors):
-				
-				class_vec = softmax(data[0, i, j, k*n_single_out+5:k*n_single_out+n_single_out])
-				objectness = sigmoid(data[0, i, j, k*n_single_out+4])
-				class_prob = objectness*class_vec		
-				
-				w = np.exp(data[0, i, j, k*n_single_out+2])*anchors[k][0]*net_scale
-				h = np.exp(data[0, i, j, k*n_single_out+3])*anchors[k][1]*net_scale
-				dx = sigmoid(data[0, i, j, k*n_single_out])
-				dy = sigmoid(data[0, i, j, k*n_single_out+1])
-				x = (j+dx)*net_scale-w/2.0
-				y = (i+dy)*net_scale-h/2.0
-				
-				classes.append(class_prob[important_classes])
-				locations.append([x, y, w, h])
-								
-	classes = np.array(classes)
-	locations = np.array(locations)
-	
-	return classes, locations
+'''对检测得到的结果进行预处理
+'''
+    locations = []
+    classes = []
+    for i in range(grid_h):
+        for j in range(grid_w):
+            for k in range(n_anchors):
+                # 当前限位框属于各个类别的概率
+                class_vec = softmax(data[0, i, j, k*n_single_out+5:k*n_single_out+n_single_out])
+                # 当前限位框是一个物体的概率
+                objectness = sigmoid(data[0, i, j, k*n_single_out+4])
+                class_prob = objectness * class_vec  # 该限位框属于各个类别的概率
+
+                # 得到图像缩放之后尺度下的绝对坐标下限位框的左上角坐标和宽度与高度
+                w = np.exp(data[0, i, j, k*n_single_out+2])*anchors[k][0]*net_scale
+                h = np.exp(data[0, i, j, k*n_single_out+3])*anchors[k][1]*net_scale
+                dx = sigmoid(data[0, i, j, k*n_single_out])
+                dy = sigmoid(data[0, i, j, k*n_single_out+1])
+                x = (j+dx)*net_scale-w/2.0
+                y = (i+dy)*net_scale-h/2.0
+
+                # 只提取选中的类别
+                classes.append(class_prob[important_classes])
+                locations.append([x, y, w, h])
+    classes = np.array(classes)
+    locations = np.array(locations)
+
+    return classes, locations
 
 def non_max_supression(classes,locations):
+'''对一幅图像的检测结果进行非最大值抑制
+'''
+    classes = np.transpose(classes)
+    indxs = np.argsort(-classes, axis=1)
+    
+    for i in range(classes.shape[0]):
+        classes[i] = classes[i][indxs[i]]
 
-	classes = np.transpose(classes)
-	indxs = np.argsort(-classes, axis=1)
+    # 执行阈值操作，即当某一个限位框属于某一类别的概率小于roi_prob时
+    # ，则将此概率设置为零，以便忽略此属于关系
+    for class_idx, class_vec in enumerate(classes):
+        for roi_idx, roi_prob in enumerate(class_vec):
+            if roi_prob < prob_th:
+                classes[class_idx][roi_idx] = 0
 
-	for i in range(classes.shape[0]):
-		classes[i] = classes[i][indxs[i]]
-
-	for class_idx, class_vec in enumerate(classes):
-		for roi_idx, roi_prob in enumerate(class_vec):
-			if roi_prob < prob_th:
-				classes[class_idx][roi_idx] = 0
-						
-	for class_idx, class_vec in enumerate(classes):
-		for roi_idx, roi_prob in enumerate(class_vec):
-			
-			if roi_prob == 0:
-				continue
-				
-			roi = locations[indxs[class_idx][roi_idx]]
-			
-			for roi_ref_idx, roi_ref_prob in enumerate(class_vec):
-
-				if roi_ref_prob == 0 or roi_ref_idx <= roi_idx:
-					continue
-
-				roi_ref = locations[indxs[class_idx][roi_ref_idx]]
-					
-				if iou(roi, roi_ref) > iou_th:
-					classes[class_idx][roi_ref_idx] = 0
-				
-	return classes, indxs
+    for class_idx, class_vec in enumerate(classes):
+        for roi_idx, roi_prob in enumerate(class_vec):
+            if roi_prob == 0:
+                continue
+            roi = locations[indxs[class_idx][roi_idx]]
+            
+            for roi_ref_idx, roi_ref_prob in enumerate(class_vec):
+                if roi_ref_prob == 0 or roi_ref_idx <= roi_idx:
+                    continue
+                roi_ref = locations[indxs[class_idx][roi_ref_idx]]
+                if iou(roi, roi_ref) > iou_th:
+                    classes[class_idx][roi_ref_idx] = 0
+    return classes, indxs
 
 def draw_objects(classes,rois,indxs,img):
 
-	scale_w = img.shape[1]/float(input_w)
-	scale_h = img.shape[0]/float(input_h)
+    scale_w = img.shape[1]/float(input_w)
+    scale_h = img.shape[0]/float(input_h)
 
-	for class_idx, class_ in enumerate(classes):
-		for loc_idx, class_prob in enumerate(class_):
-			if class_prob>0:
-
-				x = int(rois[indxs[class_idx][loc_idx]][0]*scale_w)
-				y = int(rois[indxs[class_idx][loc_idx]][1]*scale_h)
-				w = int(rois[indxs[class_idx][loc_idx]][2]*scale_w)
-				h = int(rois[indxs[class_idx][loc_idx]][3]*scale_h)
-				
-				cv2.rectangle(img, (x,y), (x+w, y+h), colors[class_idx], 4)					
-				text = names[class_idx] + ' %.2f'%(class_prob)
-				cv2.putText(img, text, (x, y-8), cv2.FONT_HERSHEY_SIMPLEX, 0.7, colors[class_idx], 2, cv2.LINE_AA)
-	return img
+    for class_idx, class_ in enumerate(classes):
+        for loc_idx, class_prob in enumerate(class_):
+            if class_prob>0:
+                # 将限位框缩放至真实图像的尺度下
+                x = int(rois[indxs[class_idx][loc_idx]][0]*scale_w)
+                y = int(rois[indxs[class_idx][loc_idx]][1]*scale_h)
+                w = int(rois[indxs[class_idx][loc_idx]][2]*scale_w)
+                h = int(rois[indxs[class_idx][loc_idx]][3]*scale_h)
+                cv2.rectangle(img, (x,y), (x+w, y+h), colors[class_idx], 4)					
+                              text = names[class_idx] + ' %.2f'%(class_prob)
+                cv2.putText(img, text, (x, y-8), cv2.FONT_HERSHEY_SIMPLEX, 
+                              0.7, colors[class_idx], 2, cv2.LINE_AA)
+    return img
 
 def read_anchors_file(file_path):
 
-	anchors = []
-	with open(file_path) as file:
-		for line in file.read().splitlines():
-			anchors.append(map(float,line.split()))
+    anchors = []
+    with open(file_path) as file:
+        for line in file.read().splitlines():
+            anchors.append(map(float,line.split()))
 
-	return np.array(anchors)
+    return np.array(anchors)
 
 def read_labels_file(file_path):
 	
-	classes, names, colors = [], [], []
-	with open(file_path) as file:
-		lines = file.read().splitlines()
-		for line in lines:
-			class_, name, color = line.split()
-			classes.append(int(class_))
-			names.append(name)
-			colors.append(eval(color))
+    classes, names, colors = [], [], []
+    with open(file_path) as file:
+    lines = file.read().splitlines()
+    for line in lines:
+        class_, name, color = line.split()
+        classes.append(int(class_))
+        names.append(name)
+        colors.append(eval(color))
 
-	return classes, names, colors
+    return classes, names, colors
 
 # --------------------------------------------------------------------------------------------------
 # --------------------------------------------------------------------------------------------------
 # --------------------------------------------------------------------------------------------------
 
 net_scale = 32.0
-grid_w, grid_h = 39, 22
+grid_w, grid_h = 39, 22  # 分别表示在宽度和高度尺度上的网格单元个数
 input_w,input_h= 1248, 704
 prob_th, iou_th = 0.7, 0.5
 
@@ -152,19 +155,19 @@ cap = cv2.VideoCapture('./video.mp4')
 
 while(cap.isOpened()):
 
-	ret, frame = cap.read()
+    ret, frame = cap.read()
 	
-	if ret == True:
+    if ret == True:
 		
-		img_for_yolo = cv2.resize(frame, (input_w, input_h))/255.0
-		d.update({image_placeholder: [img_for_yolo]})
-		data=sess.run(output, feed_dict=d)
+        img_for_yolo = cv2.resize(frame, (input_w, input_h))/255.0
+        d.update({image_placeholder: [img_for_yolo]})
+        data=sess.run(output, feed_dict=d)
 
-		classes, rois = preprocess_yolo_output(data)
-		classes, indxs = non_max_supression(classes, rois)
-		img = draw_objects(classes, rois, indxs, frame)
+        classes, rois = preprocess_yolo_output(data)
+        classes, indxs = non_max_supression(classes, rois)
+        img = draw_objects(classes, rois, indxs, frame)
 
-		cv2.imshow('image', img)
-		cv2.moveWindow('image', 0, 0)
-		key = cv2.waitKey(30)
-		if key == 27: break
+        cv2.imshow('image', img)
+        cv2.moveWindow('image', 0, 0)
+        key = cv2.waitKey(30)
+        if key == 27: break
